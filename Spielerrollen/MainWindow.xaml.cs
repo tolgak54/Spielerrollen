7f33f30f0d72797e3aaeff0ef12b05011556ad60
tolgak54 <kordon363t@htl-ottakring.ac.at>
2017-04-20 16:41:15 +0200

    die Datei wird nun gelesen. Die farben sind noch nicht fertig, wenn man auf den button click sollen sich die farben ändern.

--- Spielerrollen/MainWindow.xaml.cs
+++ Spielerrollen/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace Spielerrollen
 {
@@ -20,13 +22,63 @@ namespace Spielerrollen
     /// </summary>
     public partial class MainWindow : Window
     {
+        OpenFileDialog ofd = new OpenFileDialog();
+        List<Spieler> spieler = new List<Spieler>();
+
         public MainWindow()
         {
-            int test;
-
-            DAss ist ein test
-
             InitializeComponent();
+            //test();
         }
+
+        private void button_datei_Click(object sender, RoutedEventArgs e)
+        {
+            string line = "";
+
+
+            if (ofd.ShowDialog() == true)
+            {
+
+                StreamReader sr = new StreamReader(ofd.FileName);
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string idx = line.Substring(0, line.IndexOf(";"));
+                    listBoxAusgabe.Items.Add(line);
+                }
+                listBoxDatei.Items.Add(ofd.FileName);
+                sr.Close();
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < listBoxAusgabe.Items.Count; i++)
+            {
+                switch (spieler.Count)
+                {
+                    case 1:
+   
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+
+                        break;
+                    default:
+                        
+                        break;
+                }
+            }
+        }
+        //public void test()
+        //{
+        //FileStream f = new FileStream(@"C:\Users\tolga\Desktop\Schule\3CHIT\SEW\Projekt\Spielerrollen2.csv", FileMode.Open, FileAccess.Read);
+        //StreamReader s = new StreamReader(f);
+        //label.Content = s.ReadToEnd();       
+        //string[] sj = s.ReadToEnd().Split(';');
+        //label.Content = "Name: " + sj[0] + ", Position: " + sj[1]; 
+        //}
     }
 }
---
+++ Spielerrollen/Spieler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spielerrollen
+{
+    class Spieler
+    {
+        public string Name { get; set; }
+        //public string Position { get; set; }
+        public string Angreifer { get; set; }
+        public string Mittelfeldspieler { get; set; }
+        public string Verteidiger { get; set; }
+        public string Torwart { get; set; }
+
+        public Spieler(string name, string angreifer, string mittelfeld, string verteidiger, string torwart)
+        {
+            Name = name;
+            Angreifer = angreifer;
+            Mittelfeldspieler = mittelfeld;
+            Verteidiger = verteidiger;
+            Torwart = torwart;
+        }
+    }
+}
